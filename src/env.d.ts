a537887a5d5d2d17602fd147bd08c9462b18928e
James Hush <james@daily.co>
2022-08-01 14:30:56 +0800

    Fix dependency issues

---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_TOKBOX_API_KEY: string;
+  readonly VITE_TOKBOX_TOKEN: string;
+  readonly VITE_DAILY_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
