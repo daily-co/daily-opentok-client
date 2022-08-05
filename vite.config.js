ad0707af7582338b9186bc2aa02f649086091861
James Hush <james@daily.co>
2022-08-05 11:15:42 +0800

    Add library build config to vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,35 @@
+import { resolve } from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
 
-
-  console.log("env: ", env.VITE_TOKBOX_TOKEN);  
+  console.log("env: ", env.VITE_TOKBOX_TOKEN);
 
   return defineConfig({
     plugins: [react()],
     build: {
       outDir: "build",
+      lib: {
+        entry: resolve(__dirname, "src/shim.ts"),
+        name: "OT",
+        // the proper extensions will be added
+        fileName: "daily-tokbox",
+      },
+      // rollupOptions: {
+      //   // make sure to externalize deps that shouldn't be bundled
+      //   // into your library
+      //   external: ["vue"],
+      //   output: {
+      //     // Provide global variables to use in the UMD build
+      //     // for externalized deps
+      //     globals: {
+      //       vue: "Vue",
+      //     },
+      //   },
+      // },
     },
     server: {
       strictPort: true,
