76ced46e13aed34051fda827d9bd3ee2b9013ccf
James Hush <james@daily.co>
2022-08-08 18:09:26 +0800

    Implement OTEventEmitter

--- src/index.ts
+++ src/index.ts
@@ -19,9 +19,75 @@ export type StreamCreatedEvent = Event<"streamCreated", Session> & {
   stream: DailyStream;
 };
 
-type PublisherProperties = OT.PublisherProperties & { dailyElementId?: string };
+class OTEventEmitter<EventMap> {
+  on<EventName extends keyof EventMap>(
+    eventName: EventName,
+    callback: (event: EventMap[EventName]) => void,
+    context?: object
+  ): void;
 
-class Subscriber extends OT.OTEventEmitter<{
+  on(
+    eventName: string,
+    callback: (event: Event<string, any>) => void,
+    context?: object
+  ): void;
+
+  on(eventName: string | object, callback: any, context?: object): void {
+    if (typeof eventName === "string") {
+      ee.on(eventName, callback);
+    }
+  }
+
+  once<EventName extends keyof EventMap>(
+    eventName: EventName,
+    callback: (event: EventMap[EventName]) => void,
+    context?: object
+  ): void;
+
+  once(
+    eventName: string,
+    callback: (event: Event<string, any>) => void,
+    context?: object
+  ): void;
+
+  once(eventMap: object, context?: object): void;
+
+  once(eventName: any, callback: any, context?: any): void {
+    if (typeof eventName !== "string") {
+      throw new Error("eventName must be a string");
+    }
+    if (typeof callback !== "function") {
+      throw new Error("callback must be a function");
+    }
+    ee.once(eventName, callback);
+  }
+
+  off<EventName extends keyof EventMap>(
+    eventName?: EventName,
+    callback?: (event: EventMap[EventName]) => void,
+    context?: object
+  ): void;
+
+  off(
+    eventName?: string,
+    callback?: (event: Event<string, any>) => void,
+    context?: object
+  ): void;
+
+  off(eventMap: object, context?: object): void;
+
+  off(eventName: any, callback: any, context?: any): void {
+    if (typeof eventName !== "string") {
+      throw new Error("eventName must be a string");
+    }
+    if (typeof callback !== "function") {
+      throw new Error("callback must be a function");
+    }
+    ee.off(eventName, callback);
+  }
+}
+
+class Subscriber extends OTEventEmitter<{
   audioLevelUpdated: Event<"audioLevelUpdated", Subscriber> & {
     audioLevel: number;
   };
@@ -129,6 +195,8 @@ class Subscriber extends OT.OTEventEmitter<{
     throw new Error("Method not implemented.");
   }
 }
+
+type PublisherProperties = OT.PublisherProperties & { dailyElementId?: string };
 
 class Publisher {
   dailyElementId?: string;
