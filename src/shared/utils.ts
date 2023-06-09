33dc853d17ef11822ef5f95ea842e34dbd34cdc4
James Hush <james@daily.co>
2023-06-09 08:50:47 +0800

    Update packages and implement liza's suggestions

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -6,7 +6,6 @@ export function getOrCreateCallObject(): DailyCall {
   if (window.call) return window.call;
   window.call = DailyIframe.createCallObject({
     subscribeToTracksAutomatically: false,
-    dailyConfig: {},
   });
   return window.call;
 }
